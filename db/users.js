82aa9042b76700f0e0692386a479b845c6d9fa35
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-01-30 19:15:37 -0600

    createUser function working

--- db/users.js
+++ db/users.js
@@ -3,8 +3,19 @@ const client = require("./client");
 // database functions
 
 // user functions
-async function createUser({ username, password }) {
-  
+async function createUser({ 
+                            username, 
+                            password 
+                          }) {
+
+
+    const {rows: [users] } = await client.query(`
+    INSERT INTO users (username, password )
+    VALUES($1, $2)
+    RETURNING id, username, password;
+    `, [username, password]);
+    return users;
+
 }
 
 async function getUser({ username, password }) {
