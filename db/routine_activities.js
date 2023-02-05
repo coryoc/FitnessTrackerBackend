8e89becb9f4a7664a23dfc36bc15eca52ad59668
Cory Chaplin <cory.o.chaplin@gmail.com>
2023-02-04 19:11:17 -0600

    first draft for 5 of 6 functions written

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -24,15 +24,88 @@ async function addActivityToRoutine({
 
 }
 
-async function getRoutineActivityById(id) { }
+async function getRoutineActivityById(id) {
+  try {
+    const { rows: [routine_activity] } = await client.query(`
+      SELECT * FROM routine_activities
+      WHERE id=$1;
+    `, [id]);
+
+    return routine_activity;
+
+  } catch (error) {
+    console.log('Error executing getActivityById within activities.js');
+    throw error;
+  }
+
+ }
 
 async function getRoutineActivitiesByRoutine({ id }) { }
 
-async function updateRoutineActivity({ id, ...fields }) { }
+async function updateRoutineActivity({ id, ...fields }) {
+  
 
-async function destroyRoutineActivity(id) { }
+  const setString = Object.keys(fields).map(
+    (key, index) => `${key}=$${index + 1}`
+  ).join(`, `);
 
-async function canEditRoutineActivity(routineActivityId, userId) { }
+  if (setString.length === 0) {
+    return;
+  }
+
+
+  try {
+  const { rows: [routine_activity] } = await client.query(`
+    UPDATE routine_activities("routineId", "activityId", count, duration)
+    SET count=$2
+    SET duration=$3
+   WHERE id=$1;
+    `, Object.values(fields));
+
+  return routine_activity;
+
+} catch (error) {
+  console.log('Error updating routine_activity with ID:', id);
+  throw error;
+}}
+
+async function destroyRoutineActivity(id) {
+  try { 
+    const {rows: [routine_activity]} = await client.query(`
+    DELETE FROM routine_activities
+    WHERE id=$1;
+  `,[id]);
+
+  } catch(error) {
+    console.log('Error removing the RoutineActivity for given ID:' , id);
+    throw error;
+  }
+  
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) { 
+
+
+    const routineActivity = await getRoutineActivityById(routineActivityId);
+
+
+    if (!routineActivity) {
+      throw Error("Routine Activity does not exist with that id");
+    }
+
+      if (userId != routines . creatorId) {
+          throw Error("Only the original user is able to edit the routine activity");
+      }
+
+      else {
+        await updateRoutineActivity(routineActivityId);
+      }
+
+    return ({"message": "Routine Activity successfully updated!"})
+
+
+
+}
 
 module.exports = {
   getRoutineActivityById,
