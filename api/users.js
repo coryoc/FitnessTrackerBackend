460240d96fd40f0d924deb33d97320cfcd40cc6a
chunkay1 <fabian.hernandez1713@gmail.com>
2023-02-10 00:12:46 -0600

    partly passing /register for usersRouter, console logs show the data that's coming in. We need to pay attention to exactly what the test is expecting - if the return object isn't formatted exactly like it expects, the test will fail.

--- api/users.js
+++ api/users.js
@@ -2,7 +2,66 @@
 const express = require("express");
 const usersRouter = express.Router();
 
+const { createUser, getUser, getUserById, getUserByUsername } = require('../db');
+
+const { PasswordTooShortError, UserTakenError } = require('../errors');
+
+const jwt = require('jsonwebtoken');
+const { TOKEN_SECRET } = process.env;
+
 // POST /api/users/register
+
+usersRouter.post('/register', async (req, res, next) => {
+    const { username, password } = req.body;
+
+    try {
+        console.log('username is:', username)
+        // console.log('password is:', password)
+
+        const userCheck = await getUserByUsername(username);
+
+        console.log('usercheck is:', userCheck)
+
+        if (userCheck) {
+            next({
+                error: 'UserTakenError',
+                message: UserTakenError(username),
+                name: `${username}`
+            });
+        } else if (password.length < 8) {
+            next({
+                error: 'PasswordTooShort!',
+                message: PasswordTooShortError(),
+                name: `:[`
+            });
+        }
+
+        const newUser = await createUser({
+            username,
+            password
+        });
+
+        console.log(newUser);
+
+        // const token = jwt.sign({
+        //     id: newUser.id,
+        //     username
+        // }, process.env.TOKEN_SECRET, {
+        //     expiresIn: '1d'
+        // });
+
+        console.log('newUser is:', newUser);
+
+        res.send({
+            message: 'thank you for singing up',
+            token: 'dfasdfasd',
+            user: newUser
+        });
+
+    } catch ({ error, message, name }) {
+        next(error, message, name);
+    }
+})
 
 // POST /api/users/login
 
